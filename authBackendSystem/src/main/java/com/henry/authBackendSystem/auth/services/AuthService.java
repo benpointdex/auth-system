59b0c809ed1d510dc3a783cecfb7e8e5398a74b2
benpointdex <185585972+benpointdex@users.noreply.github.com>
2026-04-26 22:45:21 +0530

    feat: implemented user registration with password encryption

---
+++ authBackendSystem/src/main/java/com/henry/authBackendSystem/auth/services/AuthService.java
@@ -0,0 +1,11 @@
+package com.henry.authBackendSystem.auth.services;
+
+import com.henry.authBackendSystem.auth.payload.UserDto;
+
+public interface AuthService {
+    UserDto registerUser(UserDto userDto);
+
+
+    //login user
+
+}
