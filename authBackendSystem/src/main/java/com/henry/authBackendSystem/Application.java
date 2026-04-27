429b31ef7ad002e7445f896e0750ff80b29faab5
benpointdex <185585972+benpointdex@users.noreply.github.com>
2026-04-27 22:51:16 +0530

    feat: added Spring Boot application entry point

---
+++ authBackendSystem/src/main/java/com/henry/authBackendSystem/Application.java
@@ -0,0 +1,72 @@
+package com.henry.authBackendSystem;
+
+import com.henry.authBackendSystem.auth.config.AppConstants;
+import com.henry.authBackendSystem.auth.entities.Role;
+import com.henry.authBackendSystem.auth.repositories.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+@AllArgsConstructor
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+
+
+
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return (args)-> {
+			System.out.println("this is bean command line runner");
+		};
+	}
+
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//we will create some default user role
+		//ADMIN
+		//GUEST
+
+		roleRepository.findByName("ROLE_"+AppConstants.ADMIN_ROLE).ifPresentOrElse(role->{
+			System.out.println("Admin Role Already Exists: "+role.getName());
+		},()->{
+
+			Role role=new Role();
+			role.setName("ROLE_"+ AppConstants.ADMIN_ROLE);
+			role.setId(UUID.randomUUID());
+			roleRepository.save(role);
+
+		});
+
+		roleRepository.findByName("ROLE_"+AppConstants.GUEST_ROLE).ifPresentOrElse(role->{
+			System.out.println("Guest Role Already Exists: "+role.getName());
+		},()->{
+
+			Role role=new Role();
+			role.setName("ROLE_"+AppConstants.GUEST_ROLE);
+			role.setId(UUID.randomUUID());
+			roleRepository.save(role);
+
+		});
+
+
+
+	}
+
+}
